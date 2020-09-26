116707fa936ea89b4263307041f824a90fd51fba
Anmol Bajaj <hello@anmolbajaj.net>
2020-09-25 23:12:07 -0700

    Add rotating banner

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,14 +4,29 @@ import screenshot from '../../assets/images/app-screenshot.png';
 import anmol from '../../assets/images/anmol.jpg';
 import mehtab from '../../assets/images/mehtab.jpg';
 
+const textArray = ['Martial Arts Studio', 'Yoga Studio', 'Fitness Studio', 'Boxing Gym', 'Crossfit Gym'];
+
 export default class Hero extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             mobileHamburgerClicked: false
+             mobileHamburgerClicked: false,
+             textIdx: 0
         }
     }
+
+    componentDidMount() {
+        this.timeout = setInterval(() => {
+          let currentIdx = this.state.textIdx;
+          this.setState({ textIdx: currentIdx + 1 });
+        }, 3000);
+    }
+
+    componentDidUnmount() {
+        clearInterval(this.timeout);
+    }
+
 
     mobileHamburgerClickHandler = () =>  {
         console.log('clicked');
@@ -22,6 +37,8 @@ export default class Hero extends Component {
     }
     
     render() {
+        let textThatChanges = textArray[this.state.textIdx % textArray.length];
+
         return (
             <div>
                 
@@ -132,10 +149,10 @@ export default class Hero extends Component {
         <div class="text-center">
           <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
             {/* Check-in data to enrich your */}
-            Build Safer and Healthier
+            Build a Safer and Healthier
             <br />
             {/* <span class="text-indigo-600">gym or studio business</span> */}
-            <span class="text-indigo-600">Martial Arts Studios</span>
+            <span class="text-indigo-600">{textThatChanges}</span>
           </h2>
           <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
            No more paper logbook. No more membership cards. Memberly automatically tracks who visited your business, and when, using QR codes.
@@ -393,7 +410,7 @@ export default class Hero extends Component {
             </dt>
             <dd class="mt-2">
               <p class="text-base leading-6 text-gray-500">
-              We are on a mission to make it easier for you to run and manage your martial arts business. That, and we wanted an excuse to train at other places.
+              We are on a mission to make it easier for you to run and manage your fitness or martial arts studio. That, and we wanted an excuse to train at other places.
               </p>
             </dd>
           </div>
